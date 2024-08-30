df2d4dfde47db78e330fc94df788cde1fdd70859
Your Name <you@example.com>
2024-08-30 23:22:50 +0700

    Xóa toàn bộ nội dung trong nhánh sb101

--- option/h_turn_udp_proxy.go
+++
@@ -1,8 +0,0 @@
-package option
-
-type TurnRelayOptions struct {
-	ServerOptions
-	Username   string             `json:"username,omitempty"`
-	Password   string             `json:"password,omitempty"`
-	Realm string   `json:"realm,omitempty"`
-}
